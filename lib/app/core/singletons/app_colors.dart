3ae83984fbaa6065a5fb6d367ab66011957faf01
AlexFlorenco <aflorenco13@gmail.com>
2024-04-22 01:35:24 -0300

    style: Singleton AppColors

---
+++ lib/app/core/singletons/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class AppColor {
+  AppColor._();
+
+  static const Color primary = Color(0xFF369FFF);
+
+  static const Color success = Color(0xFF5CB85C);
+  static const Color error = Color(0xFFCF4949);
+
+  static const Color light = Color(0xFFFFFFFF);
+  static const Color shade = Color(0xFFAAAAAA);
+  static const Color semiTransparent = Color(0x8A000000);
+  static const Color transparent = Color(0x00000000);
+}
